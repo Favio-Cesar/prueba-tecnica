08e0a94eb062deae0f71f5ed07347c6de7101147
Favio138 <44302490+Favio138@users.noreply.github.com>
2025-04-01 12:17:10 +0200

    build [dependencies] : Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,60 @@
+import { defineNuxtConfig } from 'nuxt/config'
+import { alias } from './config/alias'
+import { cssImports } from './config/css'
+import i18nConfig from './config/i18n'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/i18n',
+    '@nuxt/image',
+    '@nuxt/test-utils/module',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/robots',
+    '@nuxt/icon',
+    '@nuxtjs/strapi',
+  ],
+  devtools: { enabled: true },
+  site: { url: 'https://alebateducation.com' },
+  ssr: false,
+  spaLoadingTemplate: false,
+  css: cssImports,
+  imports: {
+    dirs: ['core/globals'],
+  },
+  alias,
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "sass:map"; @import "assets/styles/mixin.scss";',
+        },
+      },
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: '_nuxt/[name].[ext]',
+          entryFileNames: '_nuxt/[name].js',
+          chunkFileNames: '_nuxt/[name].js',
+        },
+      },
+    },
+  },
+  i18n: {
+    ...i18nConfig,
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root',
+      fallbackLocale: 'es',
+    },
+  },
+  strapi: {
+    url: process.env.STRAPI_URL,
+    prefix: '/api',
+    version: 'v5',
+  },
+})
