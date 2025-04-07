35982b29988852c287aff05c3a6e8b78775d28fd
Favio138 <44302490+Favio138@users.noreply.github.com>
2025-04-08 00:27:39 +0200

    fix(core): Main changes

--- components/stream/content/elements/StreamInformation.vue
+++ components/stream/content/elements/StreamInformation.vue
@@ -12,8 +12,8 @@ const stream = inject<Ref<Stream>>('stream')
     <section class="stream-info__text">
       <h2 v-if="stream">{{ stream.title }}</h2>
       <h3 v-if="stream" class="highlight">{{ stream.game_name }}</h3>
-      <ul class="tags">
-        <li v-for="tag in stream?.tags || []" :key="tag" class="tag">
+      <ul class="stream-info__tags">
+        <li v-for="tag in stream?.tags || []" :key="tag" class="stream-info__tag">
           {{ tag }}
         </li>
       </ul>
@@ -44,6 +44,20 @@ const stream = inject<Ref<Stream>>('stream')
     align-items: center;
     flex-wrap: wrap;
     gap: 0.75em;
+  }
+  &__tags {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.5rem;
+    justify-content: flex-start;
+    padding: 0;
+  }
+  &__tag {
+    background-color: rgba(162, 162, 162, 0.185);
+    padding: 0.3em 0.6em;
+    list-style-type: none;
+    margin: 0.2em;
+    border-radius: 1em;
   }
 }
 
